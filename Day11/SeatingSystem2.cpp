4f3c3abc21d25f18857bab3d8ddc4161d8ed1d39
bruteforceboy <ogbonnachibuoyim12@gmail.com>
2020-12-11 06:37:51 +0100

    updated

---
+++ Day11/SeatingSystem2.cpp
@@ -0,0 +1,66 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(0);
+  freopen("SeatingSystemIn2.txt", "r", stdin);
+  vector<string> grid;
+  string s;
+  while (cin >> s) {
+    grid.push_back(s);
+  } 
+  int n = grid.size(), m = grid[0].size();
+  while (1) {
+    vector<vector<bool>> change(n, vector<bool>(m));
+    for (int i = 0; i < n; i++) {
+      for (int j = 0; j < m; j++) if (grid[i][j] != '.') {
+        int empty = 0, occupied = 0;
+        vector<int> dx{-1, 0, 1, 0, -1, 1, -1, 1}, dy{0, 1, 0, -1, -1, 1, 1, -1};
+        vector<bool> seen(8);
+        for (int x = 1; x <= n; x++) {
+          for (int k = 0; k < 8; k++) if (!seen[k]) {
+            int nx = i + x * dx[k], ny = j + x * dy[k];
+            if (nx >= 0 && nx < n && ny >= 0 && ny < m) {
+              if (grid[nx][ny] != '.')
+                seen[k] = true;
+              if (grid[nx][ny] == 'L')
+                ++empty;
+              else if (grid[nx][ny] == '#')
+                ++occupied;
+            }
+          }
+        }
+        if (grid[i][j] == 'L') {
+          if (occupied == 0) {
+            change[i][j] = true;
+          }
+        } else {
+          if (occupied >= 5) {
+            change[i][j] = true;
+          }
+        }
+      }
+    }
+    int count = 0;
+    for (int i = 0; i < n; i++) {
+      for (int j = 0; j < m; j++) {
+        if (change[i][j]) {
+          ++count;
+          grid[i][j] = grid[i][j] == '#' ? 'L' : '#';
+        }
+      }
+    }
+    if (count == 0) {
+      int res = 0;
+      for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+          res += grid[i][j] == '#';
+        }
+      }
+      cout << res << '\n';
+      return 0;
+    }
+  }
+  return 0;
+}
